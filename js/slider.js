e0a0afc76dca108bc33a6b81e7d09a450f007cec
Kethleen Gonçalves da Silva <kethleeng06@gmail.com>
2026-03-08 22:18:33 -0300

    adicionei a primeira promoção e os carrossel

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,7 @@
-const sliders = {}
+function trocarImagem(elemento){
 
-function mudarSlide(index, direcao){
+let imagemPrincipal = document.getElementById("imagem-principal");
 
-const carousel = document.querySelectorAll(`#carousel-${index} img`)
-
-if(!sliders[index]) sliders[index] = 0
-
-carousel[sliders[index]].classList.remove("active")
-
-sliders[index] += direcao
-
-if(sliders[index] >= carousel.length) sliders[index] = 0
-
-if(sliders[index] < 0) sliders[index] = carousel.length-1
-
-carousel[sliders[index]].classList.add("active")
+imagemPrincipal.src = elemento.src;
 
 }
